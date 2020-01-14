80cecd9bcbf8555afed20581e30883d79d9af026
Michael Greer <michael.j.greer1@gmail.com>
2020-01-14 09:41:46 -0600

    Started add code for sklearn based logistic regression and random forests

---
+++ lokki/model/sklearn/logistic_regression.py
@@ -0,0 +1,6 @@
+from lokki.model import ModelChoice
+
+class LogisticRegression(ModelChoice):
+
+    def __init__(self):
+        pass
---
+++ lokki/model/sklearn/random_forest.py
@@ -0,0 +1,8 @@
+from lokki.model import ModelChoice
+
+from sklearn.ensemble import RandomForestClassifier
+
+class RandomForest(ModelChoice):
+    
+    def __init__(self):
+        pass
