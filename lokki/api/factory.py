2c7283645375def1726e9d7d653a4290f1393122
greermj <michael.j.greer1@gmail.com>
2020-01-25 06:15:44 +0000

    Added factory method that will be in charge of building analysis objects

---
+++ lokki/api/factory.py
@@ -0,0 +1,12 @@
+
+
+def configure(models, target_name, transforms, dataset, metric):
+    return AnalysisFactory(models, target_name, transforms, dataset, metric)
+
+class AnalysisFactory:
+    def __init__(self, models, target_name, transforms, dataset, metric):
+        print('testing')
+
+    def run(self):
+        pass
+    
