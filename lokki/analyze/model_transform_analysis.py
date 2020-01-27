a45ceff5ec14d153ba03ea531aeff4f79a5d2bcd
Michael Greer <michael.j.greer1@gmail.com>
2020-01-27 10:54:27 -0600

    Began developing nested cross validation

--- lokki/analyze/model_transform_analysis.py
+++ lokki/analyze/model_transform_analysis.py
@@ -9,4 +9,21 @@ class ModelTransformAnalysis:
 
     # For each param in grid run transform then get and store model performance (keep track of max)
     def get_performance(self, dataset):
-        
+
+        hyperparameter_grid = self.transform_instance.hyperparameter_grid()
+
+        if hyperparameter_grid != None:
+
+            X = dataset.drop(self.parameters['target_name'], axis = 1).copy()
+            y = dataset[self.parameters['target_name']].copy()
+
+            optimal_score = None
+            
+            for grid in self.transform_instance.hyperparameter_grid():
+
+                X_train = self.transform_instance.fit_transform(grid, X, y)
+                
+
+        else:
+
+            pass
