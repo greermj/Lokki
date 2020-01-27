010064861e8268b9cdc5f1c34188b987137e09f6
Michael Greer <michael.j.greer1@gmail.com>
2020-01-27 10:57:11 -0600

    updated pca to use get_name and also changed hyperparameter grid getter to not include ncol -1

--- lokki/transform/feature_engineering/pca.py
+++ lokki/transform/feature_engineering/pca.py
@@ -1,6 +1,5 @@
 import numpy as np
-
-from sklearn.decomposition import PCA
+import sklearn as sk
 
 from lokki.transform import TransformChoice
 
@@ -9,12 +8,12 @@ class PCA(TransformChoice):
     def __init__(self, dataset_shape):
         self.dataset_shape = dataset_shape
         self.step_size = int(dataset_shape[1] * 0.20)
-        self.grid = [{'n_components' : x} for x in np.arange(dataset_shape[1], step = self.step_size) + 1]
+        self.grid = [{'n_components' : x} for x in np.arange(dataset_shape[1] - 1, step = self.step_size) + 1]
 
     def fit_transform(self, hyperparameters, X, y):
-        pass
+        return sk.decomposition.PCA(**hyperparameters).fit_transform(X)
 
-    def get_model_name(self):
+    def get_name(self):
         return 'PCA'
 
     def hyperparameter_grid(self):
