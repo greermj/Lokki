9e2faeab216bb56089671b655bc8f240a13d477c
Michael Greer <michael.j.greer1@gmail.com>
2020-01-26 22:39:21 -0600

    Added a little modeling work; still unfinished

--- lokki/model/sklearn/logistic_regression.py
+++ lokki/model/sklearn/logistic_regression.py
@@ -1,6 +1,17 @@
+from sklearn.linear_model import LogisticRegression
+
 from lokki.model import ModelChoice
 
 class LogisticRegression(ModelChoice):
 
     def __init__(self):
         pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def get_model_name(self):
+        return 'Logistic Regression'
--- lokki/model/sklearn/random_forest.py
+++ lokki/model/sklearn/random_forest.py
@@ -1,8 +1,17 @@
-from lokki.model import ModelChoice
-
 from sklearn.ensemble import RandomForestClassifier
+
+from lokki.model import ModelChoice
 
 class RandomForest(ModelChoice):
     
     def __init__(self):
         pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def get_model_name(self):
+        return 'Random Forest'
