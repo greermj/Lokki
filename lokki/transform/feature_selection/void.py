d363d4fdc4641264313bfd710b458445cf7e9383
Michael Greer <michael.j.greer1@gmail.com>
2020-01-26 22:41:01 -0600

    added class that will represent no transform; the void transform class

---
+++ lokki/transform/feature_selection/void.py
@@ -0,0 +1,15 @@
+from lokki.transform import TransformChoice
+
+class Void(TransformChoice):
+
+    def __init__(self, dataset_shape):
+        self.dataset_shape = dataset_shape
+
+    def fit_transform(self, hyperparameters, X, y):
+        pass
+
+    def get_model_name(self):
+        return 'Void'
+
+    def hyperparameter_grid(self):
+        pass
