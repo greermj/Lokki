7148663b9504f21cdcce3173d54f88de49ec1b90
Michael Greer <michael.j.greer1@gmail.com>
2020-02-25 08:47:39 -0600

    updated testing script

--- dev/testing.py
+++ dev/testing.py
@@ -2,14 +2,25 @@ import pandas as pd
 
 import lokki
 
-data = pd.read_csv('./wdbc.csv')
+# Only for testing
+import dill as pickle
+#
+''''
+data = pd.read_csv('./dev/wdbc.csv')
 
 analysis = lokki.configure(dataset = data,
                            target_name = 'Target',
-                           transforms = ['pca', 'none', 'chi_square'],
-                           models = ['random_forest', 'svm', 'logistic_regression'],
+                           transforms = ['pca', 'mutual_information', 'ica', 'factor', 'nmf', 'chi_square', 'none'],
+                           models = ['random_forest', 'decision_tree', 'extra_tree', 'ridge', 'adaboost', 'svm', 'gradient_boosting', 'lda', 'qda', 'logistic_regression'],
                            metric = 'auc')
 
 results = analysis.run()
 
-results.visualize()
+pickle.dump(results, open('results.p', 'wb'))
+'''
+results = pickle.load(open('results.p', 'rb'))
+
+lokki.plot(analysis_object = results,
+           plot_type = 'stacked',
+           #plot_type = 'enrichment',
+           output_filename = 'out.png')
