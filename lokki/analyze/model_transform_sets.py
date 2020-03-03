ebf17ca8a664ec69d793f845448c8e6eea6ca83d
greermj <michael.j.greer1@gmail.com>
2020-03-03 21:21:19 +0000

    Added class that will be in charge of managing model transform sets

---
+++ lokki/analyze/model_transform_sets.py
@@ -0,0 +1,14 @@
+class ModelTransformSets:
+    
+    def get_model_transform_sets(self):
+
+        enrichment_sets = ['factor', 'ica', 'nmf', 'pca', 'none', 
+                           'chi_square', 'mutual_information', 'random_forest', 'decision_tree', 
+                           'extra_tree', 'lda', 'qda', 'logistic_regression', 'ridge', 
+                           'adaboost', 'gradient_boosting', 'svm']
+
+        aggregate_sets  = {'tree-based' : ['decision_tree', 'extra_tree'], 
+                           'ensemble' : ['random_forest', 'adaboost', 'gradient_boosting'], 'linear' : ['svm', 'ridge'],
+                           'feature_selection' : ['mutual_information', 'none', 'chi_square'], 'feature_engineering' : ['pca', 'ica', 'factor', 'nmf']}
+
+        return enrichment_sets, aggregate_sets
