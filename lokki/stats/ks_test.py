3956d83b22b7989469f19a6a6854d8d19ebe5eff
greermj <michael.j.greer1@gmail.com>
2020-03-03 21:22:04 +0000

    Began development of ks test

---
+++ lokki/stats/ks_test.py
@@ -0,0 +1,12 @@
+from lokki.analyze.model_transform_sets import ModelTransformSets
+
+class KSTest:
+
+    def __init__(self, results):
+        self.results = results 
+        self.enrichment_sets, self.aggregate_sets = ModelTransformSets().get_model_transform_sets()
+
+        print(self.enrichment_sets)
+
+    def run(self, filename):
+        pass
