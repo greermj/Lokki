a5785da4d29814fc4451920a2b789b8b8d6dfafc
Michael Greer <michael.j.greer1@gmail.com>
2020-03-21 01:26:32 -0500

    Updated stack plot visualization to work with new results format

--- lokki/visualize/stacked.py
+++ lokki/visualize/stacked.py
@@ -4,32 +4,21 @@ import matplotlib.pyplot as plt
 
 class Stacked:
 
-    def __init__(self, results):
-        self.results = results 
+    def __init__(self, analysis_object):
+        self.analysis_object = analysis_object
 
     def run(self, filename):
 
+        data = { '_'.join(x['key']) : x['value'] for x in self.analysis_object.results}
+        y_pos = np.arange(len(data))
 
-        keys = tuple(self.results.keys())
-        y_pos = np.arange(len(keys))
-
-        scores = []
-        labels = []
-        all_results = dict()
-        
-        for label in keys:
-            scores.append(np.mean(self.results[label]))
-            labels.append(label)
-            all_results[label] = np.mean(self.results[label])
-
-        sorted_results = {k: v for k, v in sorted(all_results.items(), key=lambda item: item[1], reverse = True)}
-
+        sorted_results = {k: v for k, v in sorted(data.items(), key=lambda item: item[1], reverse = True)}
         sorted_keys = tuple(sorted_results.keys())
         sorted_values = tuple(sorted_results.values())
 
-        plt.figure(figsize=(80, 100), dpi=100)
+        plt.figure(figsize=(80, 100))
         plt.bar(y_pos, sorted_values)
         plt.xticks(y_pos, sorted_keys, rotation = 75, fontsize = 45)
-        plt.yticks(fontsize = 60)
-        plt.ylabel('AUC',fontsize=60, labelpad=80)
+        plt.yticks(fontsize = 70)
+        plt.ylabel(self.analysis_object.scoring_metric_name,fontsize=100, labelpad=80)
         plt.savefig(filename, dpi=100)
