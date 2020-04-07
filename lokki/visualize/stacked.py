c6a01079ced07461b80eaa68fcf42e9401a399f8
Michael Greer <michael.j.greer1@gmail.com>
2020-04-07 15:02:09 -0500

    Updated stack plot code

--- lokki/visualize/stacked.py
+++ lokki/visualize/stacked.py
@@ -13,12 +13,31 @@ class Stacked:
         y_pos = np.arange(len(data))
 
         sorted_results = {k: v for k, v in sorted(data.items(), key=lambda item: item[1], reverse = True)}
+
         sorted_keys = tuple(sorted_results.keys())
         sorted_values = tuple(sorted_results.values())
 
-        plt.figure(figsize=(80, 100))
+        # Output at most 40 bars in the stacked plot
+        num_bars = 40
+        if len(sorted_results) > num_bars:
+            y_pos = np.arange(num_bars)
+            sorted_keys = sorted_keys[:num_bars]
+            sorted_values = sorted_values[:num_bars]
+
+        # Output key
+        with open('key.txt', 'w') as writer:
+            for i in y_pos:
+                writer.write(str(i) + ':\t' + sorted_keys[i] + '\n')
+        
+        # Output figure 
+        plt.figure(figsize=(10,10), dpi=80)
+        plt.style.use('seaborn-darkgrid')
         plt.bar(y_pos, sorted_values)
-        plt.xticks(y_pos, sorted_keys, rotation = 75, fontsize = 45)
-        plt.yticks(fontsize = 70)
-        plt.ylabel(self.analysis_object.scoring_metric_name,fontsize=100, labelpad=80)
-        plt.savefig(filename, dpi=100)
+        plt.title('Performance Distribution')
+        plt.xticks(y_pos)
+        plt.ylabel(self.analysis_object.scoring_metric_name)
+        plt.savefig(filename)
+        return plt
+
+    def tri_color(self, filename):
+        pass
