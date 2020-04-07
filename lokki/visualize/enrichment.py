af7c7887fdd60e2f621fe0215552956eea106cdb
Michael Greer <michael.j.greer1@gmail.com>
2020-04-07 15:01:13 -0500

    updated enrichment

--- lokki/visualize/enrichment.py
+++ lokki/visualize/enrichment.py
@@ -5,9 +5,7 @@ import matplotlib.pyplot as plt
 
 from scipy import stats
 from itertools import combinations 
-
-from lokki.analyze import ModelTransformSets
-from lokki.lib import ks_test
+from scipy.stats import ks_2samp
 
 class Enrichment:
 
@@ -20,47 +18,67 @@ class Enrichment:
     
         results = dict() 
         ranked_data = self.get_ranked_list()
+        ranked_values = [x['value'] for x in ranked_data]
         dimensions = list(set([x for y in ranked_data for x in y['key']]))
 
         # Add single dimension data 
         for dimension in dimensions:
-            print(dimension)
+            #print(dimension)
             enrichment_bars = []
             for data in ranked_data:
                 if dimension in data['key']:
                     enrichment_bars.append(1)
                 else:
                     enrichment_bars.append(0)
-            print(enrichment_bars)
-            ks_stat, p_value = ks_test(ranked_data, enrichment_bars)
-            results[dimension] = {'name' : dimension, 'bars' : enrichment_bars.copy(), 'ks_stat' : ks_stat, 'p_value' : p_value}
+            #print(enrichment_bars)
+
+            enrichment_ranks = [i for i, x in enumerate(ranked_data) if enrichment_bars[i] == 1]
+            all_ranks        = [j for j in range(len(ranked_data)) if not j in enrichment_ranks]
+            ks_sign          = 1 if np.mean(enrichment_ranks) < np.median(all_ranks) else -1
+            
+            ks_stat, p_value = ks_2samp(enrichment_ranks, all_ranks) if not enrichment_ranks == [] else (0, 1) 
+            results[dimension] = {'name' : dimension, 'bars' : enrichment_bars.copy(), 'ks_stat' : ks_stat * ks_sign, 'p_value' : p_value}
 
         # Add combination dimension data
         for i in range(2, self.max_combn + 1):
 
             for combination in combinations(dimensions, i):
-                print(combination)
+                #print(combination)
                 enrichment_bars = results[combination[0]]['bars']
                 for j in range(len(combination)):
                     enrichment_bars = list(np.array(enrichment_bars) & np.array(results[combination[j]]['bars'])) 
-                print(enrichment_bars)
-                ks_stat, p_value = ks_test(ranked_data, enrichment_bars)
-                results[combination] = {'name' : combination, 'bars' : enrichment_bars.copy(), 'ks_stat' : ks_stat, 'p_value' : p_value}
+                #print(enrichment_bars)
 
-        # Sort then output 
-        sorted_results = sorted(results.items(), key = lambda x : x[1]['p_value'])
+                enrichment_ranks = [i for i, x in enumerate(ranked_data) if enrichment_bars[i] == 1]
+                all_ranks        = [j for j in range(len(ranked_data)) if not j in enrichment_ranks]
+                ks_sign          = 1 if np.mean(enrichment_ranks) < np.median(all_ranks) else -1
+
+                ks_stat, p_value = ks_2samp(enrichment_ranks, all_ranks) if not enrichment_ranks == [] else (0, 1) 
+                results[combination] = {'name' : combination, 'bars' : enrichment_bars.copy(), 'ks_stat' : ks_stat * ks_sign, 'p_value' : p_value}
+
+        # Sort then output (First by p-value, then by decreasing number of bars (ie reciprocal of length))
+        sorted_results  = [sorted(results.items(), key = lambda x : x[1]['ks_stat'], reverse = True), sorted(results.items(), key = lambda x : x[1]['ks_stat'])]
+
+        fig, ax = plt.subplots(nrows = 10, ncols = 2,figsize=(20,20))
+
+        print(ax.shape)
 
         for i in range(10):
-            key = sorted_results[i][0]
-            values = sorted_results[i][1]
+            for j in range(2):
 
-            plt.xlim(0, len(values['bars']))
-            plt.ylim(0, 1)
-            plt.bar(range(0, len(values['bars'])), values['bars'], width = 1, color = 'k')
-            plt.xticks([]),plt.yticks([]) 
-            plt.title('p-value: ' + str(round(values['p_value'], 4)) + '    stat: ' + str(round(values['ks_stat'], 4)), loc = 'left', fontsize = 10)
-            plt.ylabel(values['name'])
-            plt.savefig('sav_' + str(i) + '.png')
+                key     = sorted_results[j][i][0]
+                values  = sorted_results[j][i][1]
+
+                ax[i][j].set_xlim(0, len(values['bars']))
+                ax[i][j].set_ylim(0, 1)
+                ax[i][j].bar(range(0, len(values['bars'])), values['bars'], width = 1, color = 'k')
+                ax[i][j].set_xticks([])
+                ax[i][j].set_yticks([]) 
+                ax[i][j].set_title('p-value: ' + str(round(values['p_value'], 4)) + '    stat: ' + str(round(values['ks_stat'], 4)), loc = 'left', fontsize = 10)
+                ax[i][j].set_ylabel(values['name'] if isinstance(values['name'], str) else '\n'.join(values['name']))
+
+        fig.tight_layout(pad = 2)
+        plt.savefig('hal.png')
 
         return sorted(results.items(), key = lambda x : x[1]['p_value'])
                 
