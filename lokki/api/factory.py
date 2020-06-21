bd46157ef28e9d8c477674763b72a3bc4649a8af
Michael Greer <michael.j.greer1@gmail.com>
2020-06-21 00:30:25 -0500

    modified api to reference new naming scheme

--- lokki/api/factory.py
+++ lokki/api/factory.py
@@ -19,20 +19,20 @@ from lokki.model import LDA
 from lokki.model import QDA
 
 # Preprocessing
-from lokki.process import NoPreprocessing 
-from lokki.process import Log 
-from lokki.process import ZScore
+from lokki.data_transform import NoPreprocessing 
+from lokki.data_transform import Log 
+from lokki.data_transform import ZScore
 
 # Tranforms 
-from lokki.transform import FactorAnalysis
-from lokki.transform import ICA
-from lokki.transform import NMF
-from lokki.transform import PCA
+from lokki.feature_transform import FactorAnalysis
+from lokki.feature_transform import ICA
+from lokki.feature_transform import NMF
+from lokki.feature_transform import PCA
 
-from lokki.transform import ChiSquare
-from lokki.transform import MutualInformation
-from lokki.transform import HFE
-from lokki.transform import Void
+from lokki.feature_transform import ChiSquare
+from lokki.feature_transform import MutualInformation
+from lokki.feature_transform import HFE
+from lokki.feature_transform import Void
 
 # Visualizations 
 from lokki.visualize import Stacked 
@@ -40,7 +40,7 @@ from lokki.visualize import Enrichment
 from lokki.visualize import Hybrid 
 
 def configure(**kwargs):
-    return AnalysisFactory(kwargs['dataset'], kwargs['target_name'], kwargs['pre_processing'], kwargs['transforms'], kwargs['models'], kwargs['metric'], kwargs['taxonomy'] if 'taxonomy' in kwargs else None)
+    return AnalysisFactory(kwargs['dataset'], kwargs['target_name'], kwargs['data_transforms'], kwargs['feature_transforms'], kwargs['models'], kwargs['metric'], kwargs['taxonomy'] if 'taxonomy' in kwargs else None)
 
 def custom(**kwargs):
 
@@ -62,18 +62,18 @@ def plot(**kwargs):
     plot = None
     
     if kwargs['plot_type'].lower() == 'stacked':
-        plot = Stacked(analysis_object)
+        plot = Stacked(analysis_object, kwargs)
     if kwargs['plot_type'].lower() == 'enrichment':
-        plot = Enrichment(analysis_object)
+        plot = Enrichment(analysis_object, kwargs)
     if kwargs['plot_type'].lower() == 'hybrid':
-        plot = Hybrid(analysis_object)
+        plot = Hybrid(analysis_object, kwargs)
 
-    return plot.run(kwargs['output_filename'])
+    return plot.run()
 
 class AnalysisFactory:
     """Builds analysis objects"""
 
-    def __init__(self, dataset, target_name, pre_processing, transforms, models, metric, taxonomy):
+    def __init__(self, dataset, target_name, data_transforms, feature_transforms, models, metric, taxonomy):
         """AnalysisFactory init
 
         :param dataset: pandas dataframe containing data to analyze 
@@ -86,46 +86,46 @@ class AnalysisFactory:
         self.dataset = dataset
         self.dataset_shape = dataset.shape
         self.taxonomy = taxonomy
-        self.model_transform_tuples = list(product(transforms, models))
+        self.model_transform_tuples = list(product(feature_transforms, models))
         self.parameters  = {'target_name' : target_name, 'metric' : metric, 'num_iterations' : 5, 'num_folds' : 5}
 
         self.analysis_runs = []
 
-        for process, transform, model in list(product(pre_processing, transforms, models)):
+        for data_transform, feature_transform, model in list(product(data_transforms, feature_transforms, models)):
 
-            analysis_process = None
-            analysis_transform = None
+            analysis_data_transform = None
+            analysis_feature_transform = None
             analysis_model = None
     
-            # Preprocessing Strategy 
-            if process.lower() == 'none':
-                analysis_process = NoPreprocessing()
-            elif process.lower() == 'log':
-                analysis_process = Log()
-            elif process.lower() == 'zscore':
-                analysis_process = ZScore()
+            # Data Transformation Strategy
+            if data_transform.lower() == 'none':
+                analysis_data_transform = NoPreprocessing()
+            elif data_transform.lower() == 'log':
+                analysis_data_transform = Log()
+            elif data_transform.lower() == 'zscore':
+                analysis_data_transform = ZScore()
             else:
                 print('Error: Preprocessing method not found')
 
             # Feature Engineering Strategies 
-            if transform.lower() == 'factor':
-                analysis_transform = FactorAnalysis(self.dataset_shape)
-            elif transform.lower() == 'ica':
-                analysis_transform = ICA(self.dataset_shape)
-            elif transform.lower() == 'nmf':
-                analysis_transform = NMF(self.dataset_shape)
-            elif transform.lower() == 'pca':
-                analysis_transform = PCA(self.dataset_shape)
+            if feature_transform.lower() == 'factor':
+                analysis_feature_transform = FactorAnalysis(self.dataset_shape)
+            elif feature_transform.lower() == 'ica':
+                analysis_feature_transform = ICA(self.dataset_shape)
+            elif feature_transform.lower() == 'nmf':
+                analysis_feature_transform = NMF(self.dataset_shape)
+            elif feature_transform.lower() == 'pca':
+                analysis_feature_transform = PCA(self.dataset_shape)
 
             # Feature Selection Strategies
-            elif transform.lower() == 'none':
-                analysis_transform = Void(self.dataset_shape)
-            elif transform.lower() == 'chi_square':
-                analysis_transform = ChiSquare(self.dataset_shape)
-            elif transform.lower() == 'mutual_information':
-                analysis_transform = MutualInformation(self.dataset_shape)
-            elif transform.lower() == 'hfe':
-                analysis_transform = HFE(self.dataset_shape, self.taxonomy)
+            elif feature_transform.lower() == 'none':
+                analysis_feature_transform = Void(self.dataset_shape)
+            elif feature_transform.lower() == 'chi_square':
+                analysis_feature_transform = ChiSquare(self.dataset_shape)
+            elif feature_transform.lower() == 'mutual_information':
+                analysis_feature_transform = MutualInformation(self.dataset_shape)
+            elif feature_transform.lower() == 'hfe':
+                analysis_feature_transform = HFE(self.dataset_shape, self.taxonomy)
             else:
                 print('Error: Transform method not found')
 
@@ -153,18 +153,18 @@ class AnalysisFactory:
             else:
                 print('Error: Model method not found')
 
-            self.analysis_runs.append(ModelTransformAnalysis(analysis_process, analysis_transform, analysis_model, self.parameters))
+            self.analysis_runs.append(ModelTransformAnalysis(analysis_data_transform, analysis_feature_transform, analysis_model, self.parameters))
 
     def run(self):
 
         self.results = []
         
         for i, analysis in enumerate(self.analysis_runs):
-            current_process   = '_'.join(analysis.process_instance.get_name().lower().split(' '))
-            current_transform = '_'.join(analysis.transform_instance.get_name().lower().split(' '))
+            current_data_transform   = '_'.join(analysis.data_transform_instance.get_name().lower().split(' '))
+            current_feature_transform = '_'.join(analysis.feature_transform_instance.get_name().lower().split(' '))
             current_model     = '_'.join(analysis.model_instance.get_name().lower().split(' '))
-            print('Analyzing: ' + current_process + '_' + current_transform + '_' + current_model)
-            self.results.append({'key'   : (current_process.strip().lower(), current_transform.strip().lower(), current_model.strip().lower()), 
+            print('Analyzing: ' + current_data_transform + '_' + current_feature_transform + '_' + current_model)
+            self.results.append({'key'   : (current_data_transform.strip().lower(), current_feature_transform.strip().lower(), current_model.strip().lower()), 
                                  'value' : analysis.get_performance(self.dataset)})
 
         return AnalysisObject(self.results, self.parameters['metric'])
