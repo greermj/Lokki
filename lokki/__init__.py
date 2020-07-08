4e2003bdaf035e7dec53c88751a6000f4b0e5a45
Michael Greer <michael.j.greer1@gmail.com>
2020-07-08 00:50:59 -0500

    Updated root __init__

--- lokki/__init__.py
+++ lokki/__init__.py
@@ -5,8 +5,10 @@ from . import data_transform
 from . import feature_transform
 from . import visualize
 from . import lib
+from . import selection
 
 # Allows call on package name
 from .api.factory import configure
 from .api.factory import custom
 from .api.factory import plot
+from .api.factory import select
