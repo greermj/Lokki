bf039566da813250f5789544d75290e12c6ac1c1
Michael Greer <michael.j.greer1@gmail.com>
2020-07-09 18:24:30 -0500

    Updated model selection draft (unfinished)

--- lokki/selection/model_selection.py
+++ lokki/selection/model_selection.py
@@ -1,4 +1,5 @@
 from lokki.lib import PipelineComponents
+import sys
 
 
 class ModelSelection:
@@ -11,17 +12,71 @@ class ModelSelection:
         self.mode = mode
         self.k = k
         self.analysis_object = analysis_object
+        self.pipeline_components = PipelineComponents(self.dataset_shape, self.taxonomy)
 
+        self.results = sorted(analysis_object.results, 
+                              key = lambda x : x['value'], 
+                              reverse = True)
 
-        self.results         = sorted(analysis_object.results, 
-                                      key = lambda x : x['value'], 
-                                      reverse = True)
+        if self.results == []:
+            return
 
-        print(self.results[0])
-        print(self.mode)
+        if self.mode.lower() == 'robust':
 
-    def train(self):
-        pass
+            robust_data_transform = None
+            robust_feature_transform = None
+            robust_model = None
+
+            data_transform_counts = dict()
+            feature_transform_counts = dict()
+            model_counts = dict()
+
+            # For each sorted result dictionary, extract the pipeline components and maintain a count. Once k is hit for 
+            # each component store and return the components that hit the target count (k) first
+            for elem in self.results:
+                data_transform, feature_transform, model = elem['key']
+
+                data_transform_counts = self._update_count(data_transform, data_transform_counts)
+                feature_transform_counts = self._update_count(feature_transform, feature_transform_counts)
+                model_counts = self._update_count(model, model_counts)
+
+                if robust_data_transform == None:
+                    for x, count in data_transform_counts.items():
+                        if count == self.k:
+                            robust_data_transform = x
+
+                if robust_feature_transform == None:
+                    for x, count in feature_transform_counts.items():
+                        if count == self.k:
+                            robust_feature_transform = x
+
+                if robust_model == None:
+                    for x, count in model_counts.items():
+                        if count == self.k:
+                            robust_model = x
+
+            pipeline_build = (robust_data_transform, robust_feature_transform, robust_model)
+
+        else:
+
+            # Return the highest scoring pipeline
+            pipeline_build = self.results[0]['key']
+
+        # Retrieve components and train model 
+        analysis_data_transform    = self.pipeline_components.get_component( pipeline_build[0].lower(),    'data_transform')
+        analysis_feature_transform = self.pipeline_components.get_component( pipeline_build[1].lower(), 'feature_transform')
+        analysis_model             = self.pipeline_components.get_component( pipeline_build[2].lower(),             'model')
+
+        print(analysis_data_transform)
+        print(analysis_feature_transform)
+        print(analysis_model)
+
+    def _update_count(self, elem, dict_counts):
+        if not elem in dict_counts:
+            dict_counts[elem] = 1
+        else:
+            dict_counts[elem] += 1
+        return dict_counts 
 
     def predict(self):
         pass
