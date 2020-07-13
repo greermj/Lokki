9fafe8e4d3b1a8431afe645b60569a813a198cdf
Michael Greer <michael.j.greer1@gmail.com>
2020-07-13 02:07:05 -0500

    Added some comments showing how to add randomized cv to the models

--- lokki/model/sklearn/adaboost.py
+++ lokki/model/sklearn/adaboost.py
@@ -6,6 +6,8 @@ from sklearn.metrics import roc_auc_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 
+from sklearn.model_selection import RandomizedSearchCV
+
 from lokki.model import ModelChoice
 
 class AdaBoost(ModelChoice):
@@ -22,18 +24,27 @@ class AdaBoost(ModelChoice):
 
     def evaluate(self, parameters, X_train, X_test, y_train, y_test):
 
+        #ada = AdaBoostClassifier(random_state = 0)
+        #distribution = {'n_estimators' : [10, 50, 100, 500, 1000, 5000]}
+
+        #model = RandomizedSearchCV(ada, distribution, random_state = 0, n_iter = 1)
+        #print(model)
+
         model = AdaBoostClassifier()
         model.fit(X_train, y_train)
+
         score = None
-        pred = model.predict(X_test)
 
         if parameters['metric'] == 'auc':
+            pred = model.predict(X_test)
             score = roc_auc_score(np.array(y_test) == pred[0], pred == pred[0])
 
         elif parameters['metric'] == 'precision':
+            pred = model.predict(X_test)
             score = precision_score(np.array(y_test) == pred[0], pred == pred[0])
 
         elif parameters['metric'] == 'recall':
+            pred = model.predict(X_test)
             score = recall_score(np.array(y_test) == pred[0], pred == pred[0])
 
         return score
