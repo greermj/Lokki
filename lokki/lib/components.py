973df590068c5d6c47ad716b7ebf264ec8dbbaff
Michael Greer <michael.j.greer1@gmail.com>
2020-07-14 14:31:45 -0500

    Updated pipeline component functions

--- lokki/lib/components.py
+++ lokki/lib/components.py
@@ -98,29 +98,30 @@ class PipelineComponents:
 
     # Description: Returns a dictionary mapping the name to the component type
     def get_name_to_component_map(self):
-        return { NoPreprocessing.get_name('').lower()         : 'data_transform',
-                 Log.get_name('').lower()                     : 'data_transform',
-                 ZScore.get_name('').lower()                  : 'data_transform',
 
-                 Void.get_name('').lower()                    : 'feature_transform',
-                 ChiSquare.get_name('').lower()               : 'feature_transform',
-                 MutualInformation.get_name('').lower()       : 'feature_transform',
-                 HFE.get_name('').lower()                     : 'feature_transform',
-                 FactorAnalysis.get_name('').lower()          : 'feature_transform',
-                 ICA.get_name('').lower()                     : 'feature_transform',
-                 NMF.get_name('').lower()                     : 'feature_transform',
-                 PCA.get_name('').lower()                     : 'feature_transform',
+        def no_space(string):
+            return '_'.join(string.split(' '))
 
-                 RandomForest.get_name('').lower()            : 'model',
-                 DecisionTree.get_name('').lower()            : 'model',
-                 LDA.get_name('').lower()                     : 'model',
-                 QDA.get_name('').lower()                     : 'model',
-                 ExtraTree.get_name('').lower()               : 'model',
-                 LogisticRegressionModel.get_name('').lower() : 'model',
-                 RidgeClassifierModel.get_name('').lower()    : 'model',
-                 AdaBoost.get_name('').lower()                : 'model',
-                 GradientBoosting.get_name('').lower()        : 'model',
-                 SVM.get_name('').lower()                     : 'model'}
+        return { no_space(NoPreprocessing.get_name('').lower())         : 'data_transform',
+                 no_space(Log.get_name('').lower())                     : 'data_transform',
+                 no_space(ZScore.get_name('').lower())                  : 'data_transform',
 
+                 no_space(Void.get_name('').lower())                    : 'feature_transform',
+                 no_space(ChiSquare.get_name('').lower())               : 'feature_transform',
+                 no_space(MutualInformation.get_name('').lower())       : 'feature_transform',
+                 no_space(HFE.get_name('').lower())                     : 'feature_transform',
+                 no_space(FactorAnalysis.get_name('').lower())          : 'feature_transform',
+                 no_space(ICA.get_name('').lower())                     : 'feature_transform',
+                 no_space(NMF.get_name('').lower())                     : 'feature_transform',
+                 no_space(PCA.get_name('').lower())                     : 'feature_transform',
 
-
+                 no_space(RandomForest.get_name('').lower())            : 'model',
+                 no_space(DecisionTree.get_name('').lower())            : 'model',
+                 no_space(LDA.get_name('').lower())                     : 'model',
+                 no_space(QDA.get_name('').lower())                     : 'model',
+                 no_space(ExtraTree.get_name('').lower())               : 'model',
+                 no_space(LogisticRegressionModel.get_name('').lower()) : 'model',
+                 no_space(RidgeClassifierModel.get_name('').lower())    : 'model',
+                 no_space(AdaBoost.get_name('').lower())                : 'model',
+                 no_space(GradientBoosting.get_name('').lower())        : 'model',
+                 no_space(SVM.get_name('').lower())                     : 'model'}
